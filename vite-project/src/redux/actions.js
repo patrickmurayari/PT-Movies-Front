f18399c9454e9142471271d604854d50037cc91d
patrickmurayari <patrickmurayari03@gmail.com>
2023-09-27 14:50:04 -0300

    add change

--- vite-project/src/redux/actions.js
+++ vite-project/src/redux/actions.js
@@ -2,10 +2,11 @@ import { GET_MOVIES, PREV_PAGE, NEXT_PAGE } from "./typeActions";
 import axios from "axios";
 
 export function getMovies() {
+    
     return async function (dispatch) {
         console.log('actiopns');
         try {
-            var json = await axios.get(`http://localhost:3001/movie/all`)
+            var json = await axios.get(`https://pt-movies-back.vercel.app/movie/all`)
             console.log('::JSON.DATA::', json.data);
             return dispatch({
                 type: GET_MOVIES,
